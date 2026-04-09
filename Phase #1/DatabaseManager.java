3e5292922e0475164112996f79196c2a1711a705
wesgav9989 <wesgav9989@students.ecpi.edu>
2026-04-09 09:07:53 -0400

    DatabaseManager.java commit
    This class outlines the planned structure for handling SQLite database operations related to employee data.

---
+++ Phase #1/DatabaseManager.java
@@ -0,0 +1,42 @@
+/*
+ * Name: Wesley Gavitt
+ * Date: 04/09/2026
+ * Purpose: This class will handle SQLite database operations
+ * in later phases of the project.
+ * 
+ * NOTE:
+ * Database functionality is not fully implemented in Week 1.
+ * This class is included to show planned structure for storing
+ * and retrieving employee data using SQLite.
+ */
+
+public class DatabaseManager {
+
+    public DatabaseManager() {
+        // Future implementation will establish database connection
+    }
+
+    public void connect() {
+        // Will connect to SQLite database
+    }
+
+    public void createTable() {
+        // Will create employee table if it does not exist
+    }
+
+    public void insertEmployee(Employee employee) {
+        // Will insert employee into database
+    }
+
+    public void updateEmployee(Employee employee) {
+        // Will update employee in database
+    }
+
+    public void deleteEmployee(int employeeId) {
+        // Will delete employee from database
+    }
+
+    public void getAllEmployees() {
+        // Will retrieve employees from database
+    }
+}
